4a3542031ccdf0b1847eeef954ab35e077cfb2f5
Leonomics <leocv97@gmail.com>
2022-10-18 17:32:07 +0200

    add seeder

--- database/seeds/StudentSeeder.php
+++ database/seeds/StudentSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Student;
+use Faker\Generator as Faker;
+use Faker\Factory as Factory;
 
 class StudentSeeder extends Seeder
 {
@@ -9,8 +12,21 @@ class StudentSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+
+    public function run(Faker $faker, Factory $factory)
     {
-        //
+        for($i=0; $i<50; $i++){
+            $nS = new Student();
+            $nS->name = $faker->firstName();
+            $nS->surname = $faker->lastName();
+            $nS->registration_number = $faker->unique()->randomNumber(7, true);
+            $nS->date_of_birth = $faker->date();
+            $nS->enrolment_date = $faker->date();
+            $nS->email = $faker->unique()->email();
+            $nS->telephone_number = $faker->unique()->bothify('##############');
+            $nS->fiscal_code = $factory::create("it_IT")->taxId();
+
+            $nS->save();
+        }
     }
 }
