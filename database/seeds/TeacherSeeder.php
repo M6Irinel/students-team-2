fd1bd3f589e0435422e5e0f930751c768eb95190
masfreddy93 <masieronicola@gmail.com>
2022-10-25 16:24:22 +0200

    add teachers migration & seeder + setup

---
+++ database/seeds/TeacherSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Teacher;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TeacherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        
+        for($i=0; $i<200; $i++){
+            $t = new Teacher();
+            $t->name = $faker->firstName();
+            $t->surname = $faker->lastName();
+            $t->email = $faker->email();
+            $t->telephone = $faker->numerify('+## ### #######');
+
+            $t->save();
+        }
+
+    }
+}
--- public/js/app.js
+++ public/js/app.js
@@ -49715,8 +49715,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\Boolean\laravel-progetto-condiviso\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\Boolean\laravel-progetto-condiviso\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/students-team-2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/students-team-2/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
