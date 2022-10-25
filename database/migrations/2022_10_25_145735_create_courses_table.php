24b0bf62df6a44934122928e20c041719153a839
M6Irinel <momoloacairinel666@gmail.com>
2022-10-25 17:05:59 +0200

    add migration teacher

---
+++ database/migrations/2022_10_25_145735_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $t) {
+            $t->id();
+            $t->unsignedBigInteger('degree_id');
+            $t->string('name');
+            $t->text('description')->nullable();
+            $t->string('period');
+            $t->tinyInteger('year');
+            $t->tinyInteger('cfu');
+            $t->string('website')->nullable();
+            $t->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            //
+        });
+    }
+}
