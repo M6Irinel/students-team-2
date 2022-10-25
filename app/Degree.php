b1fe38dc96419b35bfb59975080d6b4cbfa14c14
FabioLongoni <longoni.fabio90@gmail.com>
2022-10-25 16:36:27 +0200

    add controller+model+relation

--- app/Degree.php
+++ app/Degree.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Degree extends Model
 {
-    //
+    public function students() {
+        return $this->hasMany('App\Student');
+    }
+
+    public function courses() {
+        return $this->hasMany('App\Course');
+    }
 }
